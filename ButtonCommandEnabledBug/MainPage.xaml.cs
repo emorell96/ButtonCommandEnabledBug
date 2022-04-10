54d32b994bc194811448c8765a045b193f3e99ba
Henry <emorell96@hotmail.com>
2022-04-10 13:10:33 +0200

    Add project files.

---
+++ ButtonCommandEnabledBug/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace ButtonCommandEnabledBug;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public Command Command { get; set; }
+	public MainPage()
+	{
+		BindingContext = this;
+		Command = new Command(() => OnCounterClicked());
+		InitializeComponent();
+		
+	}
+
+	private void OnCounterClicked()
+	{
+		count++;
+		CounterLabel.Text = $"Current count: {count}";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
+	}
+}
+
